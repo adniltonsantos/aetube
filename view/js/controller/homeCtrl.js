95c1348c365cc6d31ee1896a74add9428077f63e
root <guilhermebritto.prof@gmail.com>
2017-02-09 18:39:06 -0200

    correção de problema

--- view/js/controller/homeCtrl.js
+++
@@ -1,9 +0,0 @@
-app.controller('homeCtrl', function($scope , $location, usuarioAPI){
-
-	$scope.logando = function (values, formAut){
-	
-   		$scope.activePath = $location.path('/associado');
-
-	};
-
-});
